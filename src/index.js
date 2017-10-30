838ffb403a477dba2f51dff61f4224fc20999b34
carlos <hhx@lejurobot.com>
2017-10-30 15:39:11 +0800

    get next staturday

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+var moment = require('moment');
+// var lodash = require('lodash');
+
 export const fn = ({ term, display }) => {
   // Put your plugin code here
-  display({
-    title: `You've entered ${term}`
-  })
+  if(term === "sat"){
+    display({
+      title: `You've entered ${term} hi carlo`
+    });
+  }
+};
+
+
+function getStaturdayList(length){
+  var now = moment();
+  var index = 0;
+  var stat_lists = [];
+  if(length <= 0){
+    return [];
+  }
+  while(index < length){
+    isoWeekday = now.isoWeekday();
+    if(isoWeekday == 6){
+      stat_lists[stat_lists.length] = now.clone();
+      index++;
+    }
+    now =  now.add(1, 'days');
+  }
+  return stat_lists;
 }
